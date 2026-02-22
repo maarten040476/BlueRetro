abcf75e03ea47707050dd1f2e3a7c7c9b0c7baf2
maarten040476 <maarten040476@gmail.com>
2026-02-22 19:46:13 +0100

    Update wired.c

--- main/adapter/wired/wired.c
+++ main/adapter/wired/wired.c
@@ -28,29 +28,29 @@
 
 static from_generic_t from_generic_func[WIRED_MAX] = {
     NULL, /* WIRED_AUTO */
-    para_1p_from_generic, /* PARALLEL_1P */
-    para_2p_from_generic, /* PARALLEL_2P */
-    npiso_from_generic, /* NES */
-    pce_from_generic, /* PCE */
-    genesis_from_generic, /* GENESIS */
-    npiso_from_generic, /* SNES */
+    NULL, /* PARALLEL_1P */
+    NULL, /* PARALLEL_2P */
+    NULL, /* NES */
+    NULL, /* PCE */
+    NULL, /* GENESIS */
+    NULL, /* SNES */
     NULL, /* CDI */
     NULL, /* CD32 */
-    real_from_generic, /* REAL_3DO */
-    jag_from_generic, /* JAGUAR */
-    ps_from_generic, /* PSX */
-    saturn_from_generic, /* SATURN */
-    pcfx_from_generic, /* PCFX */
-    jvs_from_generic, /* JVS */
-    n64_from_generic, /* N64 */
-    dc_from_generic, /* DC */
-    ps_from_generic, /* PS2 */
+    NULL, /* REAL_3DO */
+    NULL, /* JAGUAR */
+    NULL, /* PSX */
+    NULL, /* SATURN */
+    NULL, /* PCFX */
+    NULL, /* JVS */
+    NULL, /* N64 */
+    NULL, /* DC */
+    NULL, /* PS2 */
     gc_from_generic, /* GC */
-    wii_from_generic, /* WII_EXT */
-    npiso_from_generic, /* VB */
-    para_1p_from_generic, /* PARALLEL_1P_OD */
-    para_2p_from_generic, /* PARALLEL_2P_OD */
-    sea_from_generic, /* SEA_BOARD */
+    NULL, /* WII_EXT */
+    NULL, /* VB */
+    NULL, /* PARALLEL_1P_OD */
+    NULL, /* PARALLEL_2P_OD */
+    NULL, /* SEA_BOARD */
 };
 
 static fb_to_generic_t fb_to_generic_func[WIRED_MAX] = {
@@ -60,18 +60,18 @@ static fb_to_generic_t fb_to_generic_func[WIRED_MAX] = {
     NULL, /* NES */
     NULL, /* PCE */
     NULL, /* GENESIS */
-    npiso_fb_to_generic, /* SNES */
+    NULL, /* SNES */
     NULL, /* CDI */
     NULL, /* CD32 */
     NULL, /* REAL_3DO */
     NULL, /* JAGUAR */
-    ps_fb_to_generic, /* PSX */
+    NULL, /* PSX */
     NULL, /* SATURN */
     NULL, /* PCFX */
     NULL, /* JVS */
-    n64_fb_to_generic, /* N64 */
-    dc_fb_to_generic, /* DC */
-    ps_fb_to_generic, /* PS2 */
+    NULL, /* N64 */
+    NULL, /* DC */
+    NULL, /* PS2 */
     gc_fb_to_generic, /* GC */
     NULL, /* WII_EXT */
     NULL, /* VB */
@@ -82,56 +82,56 @@ static fb_to_generic_t fb_to_generic_func[WIRED_MAX] = {
 
 static meta_init_t meta_init_func[WIRED_MAX] = {
     NULL, /* WIRED_AUTO */
-    para_1p_meta_init, /* PARALLEL_1P */
-    para_2p_meta_init, /* PARALLEL_2P */
-    npiso_meta_init, /* NES */
-    pce_meta_init, /* PCE */
-    genesis_meta_init, /* GENESIS */
-    npiso_meta_init, /* SNES */
+    NULL, /* PARALLEL_1P */
+    NULL, /* PARALLEL_2P */
+    NULL, /* NES */
+    NULL, /* PCE */
+    NULL, /* GENESIS */
+    NULL, /* SNES */
     NULL, /* CDI */
     NULL, /* CD32 */
-    real_meta_init, /* REAL_3DO */
-    jag_meta_init, /* JAGUAR */
-    ps_meta_init, /* PSX */
-    saturn_meta_init, /* SATURN */
-    pcfx_meta_init, /* PCFX */
-    jvs_meta_init, /* JVS */
-    n64_meta_init, /* N64 */
-    dc_meta_init, /* DC */
-    ps_meta_init, /* PS2 */
+    NULL, /* REAL_3DO */
+    NULL, /* JAGUAR */
+    NULL, /* PSX */
+    NULL, /* SATURN */
+    NULL, /* PCFX */
+    NULL, /* JVS */
+    NULL, /* N64 */
+    NULL, /* DC */
+    NULL, /* PS2 */
     gc_meta_init, /* GC */
-    wii_meta_init, /* WII_EXT */
-    npiso_meta_init, /* VB */
-    para_1p_meta_init, /* PARALLEL_1P_OD */
-    para_2p_meta_init, /* PARALLEL_2P_OD */
-    sea_meta_init, /* SEA_BOARD */
+    NULL, /* WII_EXT */
+    NULL, /* VB */
+    NULL, /* PARALLEL_1P_OD */
+    NULL, /* PARALLEL_2P_OD */
+    NULL, /* SEA_BOARD */
 };
 
 static DRAM_ATTR buffer_init_t buffer_init_func[WIRED_MAX] = {
     NULL, /* WIRED_AUTO */
-    para_1p_init_buffer, /* PARALLEL_1P */
-    para_2p_init_buffer, /* PARALLEL_2P */
-    npiso_init_buffer, /* NES */
-    pce_init_buffer, /* PCE */
-    genesis_init_buffer, /* GENESIS */
-    npiso_init_buffer, /* SNES */
+    NULL, /* PARALLEL_1P */
+    NULL, /* PARALLEL_2P */
+    NULL, /* NES */
+    NULL, /* PCE */
+    NULL, /* GENESIS */
+    NULL, /* SNES */
     NULL, /* CDI */
     NULL, /* CD32 */
-    real_init_buffer, /* REAL_3DO */
-    jag_init_buffer, /* JAGUAR */
-    ps_init_buffer, /* PSX */
-    saturn_init_buffer, /* SATURN */
-    pcfx_init_buffer, /* PCFX */
-    jvs_init_buffer, /* JVS */
-    n64_init_buffer, /* N64 */
-    dc_init_buffer, /* DC */
-    ps_init_buffer, /* PS2 */
+    NULL, /* REAL_3DO */
+    NULL, /* JAGUAR */
+    NULL, /* PSX */
+    NULL, /* SATURN */
+    NULL, /* PCFX */
+    NULL, /* JVS */
+    NULL, /* N64 */
+    NULL, /* DC */
+    NULL, /* PS2 */
     gc_init_buffer, /* GC */
-    wii_init_buffer, /* WII_EXT */
-    npiso_init_buffer, /* VB */
-    para_1p_init_buffer, /* PARALLEL_1P_OD */
-    para_2p_init_buffer, /* PARALLEL_2P_OD */
-    sea_init_buffer, /* SEA_BOARD */
+    NULL, /* WII_EXT */
+    NULL, /* VB */
+    NULL, /* PARALLEL_1P_OD */
+    NULL, /* PARALLEL_2P_OD */
+    NULL, /* SEA_BOARD */
 };
 
 int32_t wired_meta_init(struct wired_ctrl *ctrl_data) {
@@ -280,4 +280,3 @@ void IRAM_ATTR wired_gen_turbo_mask_axes8(struct wired_data *wired_data, uint8_t
         }
     }
 }
-
