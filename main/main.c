eb82dfd670745b1aa21509e847e58d508acf1431
maarten040476 <maarten040476@gmail.com>
2026-02-23 19:51:30 +0100

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,7 +2,6 @@
  * Copyright (c) 2019-2024, Jacques Gagnon
  * SPDX-License-Identifier: Apache-2.0
  */
-
 #include <string.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -15,7 +14,6 @@
 #include "system/core0_stall.h"
 #include "system/delay.h"
 #include "system/fs.h"
-#include "system/led.h"
 #include "adapter/adapter.h"
 #include "adapter/adapter_debug.h"
 #include "adapter/config.h"
@@ -41,7 +39,6 @@ static void wired_init_task(void) {
         delay_us(1000);
     }
     detect_deinit();
-
     if (wired_adapter.system_id >= 0) {
         ets_printf("# Detected system : %d: %s\n", wired_adapter.system_id, wired_get_sys_name());
     }
@@ -49,20 +46,16 @@ static void wired_init_task(void) {
     wired_adapter.system_id = HARDCODED_SYS;
     ets_printf("# Hardcoded system : %d: %s\n", wired_adapter.system_id, wired_get_sys_name());
 #endif
-
     while (config.magic != CONFIG_MAGIC) {
         delay_us(1000);
     }
-
     if (config.global_cfg.system_cfg < WIRED_MAX && config.global_cfg.system_cfg != WIRED_AUTO) {
         wired_adapter.system_id = config.global_cfg.system_cfg;
         ets_printf("# Config override system : %d: %s\n", wired_adapter.system_id, wired_get_sys_name());
     }
-
     for (uint32_t i = 0; i < WIRED_MAX_DEV; i++) {
         adapter_init_buffer(i);
     }
-
     struct raw_fb fb_data = {0};
     const char *sysname = wired_get_sys_name();
     fb_data.header.wired_id = 0;
@@ -70,7 +63,6 @@ static void wired_init_task(void) {
     fb_data.header.data_len = strlen(sysname);
     memcpy(fb_data.data, sysname, fb_data.header.data_len);
     adapter_q_fb(&fb_data);
-
     if (wired_adapter.system_id < WIRED_MAX) {
         wired_bare_init(chip_package);
     }
@@ -78,58 +70,41 @@ static void wired_init_task(void) {
 
 static void wl_init_task(void *arg) {
     uint32_t err = 0;
-
     const esp_partition_t *running = esp_ota_get_running_partition();
     esp_ota_img_states_t ota_state;
     esp_ota_get_state_partition(running, &ota_state);
-
     chip_package = esp_efuse_get_pkg_ver();
-
 #ifdef CONFIG_BLUERETRO_COVERAGE
     cov_init();
 #endif
-
-    err_led_init(chip_package);
-
     core0_stall_init();
-
 #ifndef CONFIG_BLUERETRO_QEMU
     if (fs_init()) {
-        err_led_set();
         err = 1;
         printf("FS init fail!\n");
     }
 #endif
-
-    /* Initialize NVS for PHY cal and default config */
     int32_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-
     config_init(DEFAULT_CFG);
     mc_init_mem();
-
 #ifndef CONFIG_BLUERETRO_BT_DISABLE
     if (bt_host_init()) {
-        err_led_set();
         err = 1;
         printf("Bluetooth init fail!\n");
     }
 #endif
-
 #ifndef CONFIG_BLUERETRO_QEMU
     mc_init();
-
     sys_mgr_init(chip_package);
 #endif
-
 #ifdef CONFIG_BLUERETRO_WS_CMDS
     ws_srv_init();
 #endif
-
     if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
         if (err) {
             printf("Boot error on pending img, rollback to the previous version.");
@@ -143,10 +118,8 @@ static void wl_init_task(void *arg) {
     vTaskDelete(NULL);
 }
 
-void app_main()
-{
+void app_main() {
     adapter_init();
-
     start_app_cpu(wired_init_task);
     xTaskCreatePinnedToCore(wl_init_task, "wl_init_task", 2560, NULL, 10, NULL, 0);
 }
