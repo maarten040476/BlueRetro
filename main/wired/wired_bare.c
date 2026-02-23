5820705ae52d362304eeb3266f65cf971da7394b
maarten040476 <maarten040476@gmail.com>
2026-02-23 18:45:58 +0100

    Update wired_bare.c

--- main/wired/wired_bare.c
+++ main/wired/wired_bare.c
@@ -4,16 +4,12 @@
  */
 
 #include <stdint.h>
-#include <stddef.h>
 #include "nsi.h"
 #include "adapter/adapter.h"
 #include "wired_bare.h"
 
 #define SPI_LL_RST_MASK (SPI_OUT_RST | SPI_IN_RST | SPI_AHBM_RST | SPI_AHBM_FIFO_RST)
 #define SPI_LL_UNUSED_INT_MASK  (SPI_INT_EN | SPI_SLV_WR_STA_DONE | SPI_SLV_RD_STA_DONE | SPI_SLV_WR_BUF_DONE | SPI_SLV_RD_BUF_DONE)
-
-typedef void (*wired_init_t)(uint32_t package);
-typedef void (*wired_port_cfg_t)(uint16_t mask);
 
 static const char *sys_name[WIRED_MAX] = {
     "AUTO",
@@ -42,69 +38,15 @@ static const char *sys_name[WIRED_MAX] = {
     "SEA Board",
 };
 
-static const wired_init_t wired_init[WIRED_MAX] = {
-    NULL, /* WIRED_AUTO */
-    NULL, /* PARALLEL_1P */
-    NULL, /* PARALLEL_2P */
-    NULL, /* NES */
-    NULL, /* PCE */
-    NULL, /* GENESIS */
-    NULL, /* SNES */
-    NULL, /* CDI */
-    NULL, /* CD32 */
-    NULL, /* REAL_3DO */
-    NULL, /* JAGUAR */
-    NULL, /* PSX */
-    NULL, /* SATURN */
-    NULL, /* PCFX */
-    NULL, /* JVS */
-    NULL, /* N64 */
-    NULL, /* DC */
-    NULL, /* PS2 */
-    nsi_init, /* GC */
-    NULL, /* WII_EXT */
-    NULL, /* VB */
-    NULL, /* PARALLEL_1P_OD */
-    NULL, /* PARALLEL_2P_OD */
-    NULL, /* SEA_BOARD */
-};
-
-static const wired_port_cfg_t wired_port_cfg[WIRED_MAX] = {
-    NULL, /* WIRED_AUTO */
-    NULL, /* PARALLEL_1P */
-    NULL, /* PARALLEL_2P */
-    NULL, /* NES */
-    NULL, /* PCE */
-    NULL, /* GENESIS */
-    NULL, /* SNES */
-    NULL, /* CDI */
-    NULL, /* CD32 */
-    NULL, /* REAL_3DO */
-    NULL, /* JAGUAR */
-    NULL, /* PSX */
-    NULL, /* SATURN */
-    NULL, /* PCFX */
-    NULL, /* JVS */
-    NULL, /* N64 */
-    NULL, /* DC */
-    NULL, /* PS2 */
-    nsi_port_cfg, /* GC */
-    NULL, /* WII_EXT */
-    NULL, /* VB */
-    NULL, /* PARALLEL_1P_OD */
-    NULL, /* PARALLEL_2P_OD */
-    NULL, /* SEA_BOARD */
-};
-
 void wired_bare_init(uint32_t package) {
-    if (wired_init[wired_adapter.system_id]) {
-        wired_init[wired_adapter.system_id](package);
+    if (wired_adapter.system_id == GC) {
+        nsi_init(package);
     }
 }
 
 void wired_bare_port_cfg(uint16_t mask) {
-    if (wired_port_cfg[wired_adapter.system_id]) {
-        wired_port_cfg[wired_adapter.system_id](mask);
+    if (wired_adapter.system_id == GC) {
+        nsi_port_cfg(mask);
     }
 }
 
@@ -116,8 +58,8 @@ void spi_init(struct spi_cfg *cfg) {
     cfg->hw->clock.val = 0;
     cfg->hw->user.val = 0;
     cfg->hw->ctrl.val = 0;
-    cfg->hw->slave.wr_rd_buf_en = 1; //no sure if needed
-    cfg->hw->user.doutdin = 1; //we only support full duplex
+    cfg->hw->slave.wr_rd_buf_en = 1;
+    cfg->hw->user.doutdin = 1;
     cfg->hw->user.sio = 0;
     cfg->hw->slave.slave_mode = 1;
     cfg->hw->dma_conf.val |= SPI_LL_RST_MASK;
@@ -127,11 +69,9 @@ void spi_init(struct spi_cfg *cfg) {
     cfg->hw->slave.sync_reset = 1;
     cfg->hw->slave.sync_reset = 0;
 
-    //use all 64 bytes of the buffer
     cfg->hw->user.usr_miso_highpart = 0;
     cfg->hw->user.usr_mosi_highpart = 0;
 
-    //Disable unneeded ints
     cfg->hw->slave.val &= ~SPI_LL_UNUSED_INT_MASK;
 
     cfg->hw->ctrl.wr_bit_order = cfg->write_bit_order;
